211728b5df3bb24ec3e31c72799aee39bf59a2b6
Demorgon <befargon@gmail.com>
2016-09-03 18:48:03 +0300

    0.3.3
    optimization of animation

--- js/common.js
+++ js/common.js
@@ -10,14 +10,37 @@ $(document).ready(function() {
     });   
 });   
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "18%";
-    document.getElementById("main").style.marginLeft = "18%";
-    document.querySelector(".nav_icon").style.marginLeft = "18%";
-}
+//function openNav() {
+//    document.getElementById("mySidenav").style.width = "18%";
+//    document.getElementById("main").style.marginLeft = "18%";
+//    document.querySelector(".nav_icon").style.marginLeft = "18%";
+//}
+//
+//function closeNav() {
+//    document.getElementById("mySidenav").style.width = "0";
+//    document.getElementById("main").style.marginLeft = "0";
+//    document.querySelector(".nav_icon").style.marginLeft = "0";
+//}
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.querySelector(".nav_icon").style.marginLeft = "0";
+var myMenu = document.querySelector(".menu_container");
+var oppMenu = document.querySelector(".nav_icon");
+var body = document.querySelector(".content_container");
+
+oppMenu.addEventListener("click", toggleClassMenu, false);
+body.addEventListener("click", closeMenu, false);
+
+function closeMenu(){
+    myMenu.classList.remove('menu--visible');
+    oppMenu.style.marginLeft = "0";
+}
+function toggleClassMenu() {	
+	if(!myMenu.classList.contains("menu--visible")) {		
+		myMenu.classList.add("menu--visible");
+        oppMenu.style.marginLeft = "280px";
+//        content.style.marginLeft = "280px";
+	} else {
+		myMenu.classList.remove('menu--visible');
+        oppMenu.style.marginLeft = "0";
+//        content.style.marginLeft = "0%";
+	}	
 }
