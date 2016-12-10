c11919c486a4162702b8ea3c5682e804d4651ab4
Demorgon <befargon@gmail.com>
2016-12-10 03:42:34 +0200

    0.5
    Добавлена анимация для третьей страницы

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     function heightDetect() {
         $(".full_page").css("height", $(window).height());
     };
-    
+
     heightDetect();
     
     $(window).resize(function() {
@@ -10,6 +10,9 @@ $(document).ready(function() {
     });  
     
     mouse_animation();
+    skills_animation();
+    
+
 });   
 
 
@@ -21,4 +24,26 @@ function mouse_animation(){
                 duration: 500, 
                 complete: mouse_animation});
         }});
-}
+};
+let skills_animation_flag = true; 
+function skills_animation(){
+        if (skills_animation_flag === true){
+            $(".skills_item").css("opacity", "0");
+            skills_animation_flag = false;
+            $(".skills_item:eq(0)").animate({opacity: '1'}, 500, function () {
+            $(this).next().delay(500).animate({opacity: '1'}, 500, arguments.callee);
+            }); 
+        }
+        else {
+            if (skills_animation_flag === false && $(".skills_item:last-child").css("opacity") == "1"){
+                skills_animation_flag = true;
+            }
+        }
+           
+};
+
+
+
+
+
+
