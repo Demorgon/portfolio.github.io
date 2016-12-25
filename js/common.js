8e0a33840dd06715dbbf59470312f3616d1b2bd7
Demorgon <befargon@gmail.com>
2016-12-25 03:22:13 +0200

    0.7

--- js/common.js
+++ js/common.js
@@ -3,24 +3,20 @@ $(document).ready(function() {
         $(".full_page").css("height", $(window).height());
     };
 
-    heightDetect();
-    svg_position_func();
+//    heightDetect();
+    
     $(window).resize(function() {
-        svg_position_func();
         heightDetect();
     });  
-    
-    mouse_animation();
-    skills_animation();
 });   
 
 
-function mouse_animation(){
-    $('.circle').animate({'top': '40%'}, {
-        duration: 500, 
-        complete: function() {
-            $('.circle').animate({'top': '5%'}, {
-                duration: 500, 
-                complete: mouse_animation});
-        }});
-};
+//function mouse_animation(){
+//    $('.circle').animate({'top': '40%'}, {
+//        duration: 500, 
+//        complete: function() {
+//            $('.circle').animate({'top': '5%'}, {
+//                duration: 500, 
+//                complete: mouse_animation});
+//        }});
+//};
